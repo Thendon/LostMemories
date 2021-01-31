58113645bfc99b92762f6547353e13f81233db0b
Florian Herrmann <florianherrmann@web.de>
2021-01-31 13:30:16 +0100

    memories, win

--- Assets/Scripts/CamAttachment.cs
+++ Assets/Scripts/CamAttachment.cs
@@ -6,5 +6,21 @@ using UnityEngine;
 public class CamAttachment : MonoBehaviour
 {
     [SerializeField]
-    Vector3 offset;
+    Vector3 posOffset = Vector3.zero;
+    [SerializeField]
+    Quaternion rotOffset = Quaternion.identity;
+    [SerializeField]
+    public Attraction attraction = null;
+
+    public Vector3 GetPosition()
+    {
+        return transform.TransformPoint(posOffset);
+        //return transform.position + transform.forward * posOffset.z + transform.right * posOffset.x + transform.up * posOffset.y;
+    }
+
+    public Quaternion GetRotation()
+    {
+        //evll anders herum :D
+        return transform.rotation * rotOffset;
+    }
 }
