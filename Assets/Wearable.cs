e78be43a1d8e061c456b7a988d9351ce7a27742d
Melvanus <melvinstubbe@googlemail.com>
2021-02-19 16:56:18 +0100

    hatshatshatshats

--- Assets/Wearable.cs
+++ Assets/Wearable.cs
@@ -9,15 +9,46 @@ public class Wearable : MonoBehaviour
     public ParentConstraint parentContraint;
     public XRGrabInteractable grabInteractable;
 
+    private WearableAttachmentPoint attachmentPoint;
+
+    private bool isWorn = false;
+    private bool isInteracted = false;
+
     public void Awake()
     {
         parentContraint = GetComponentInParent<ParentConstraint>();
         grabInteractable = GetComponentInParent<XRGrabInteractable>();
     }
 
+    public void OnGrabInteractableActivated(SelectEnterEventArgs args)
+    {
+        Debug.Log("Activated");
+
+        isInteracted = true;
+        if (isWorn)
+        {
+            var source = parentContraint.GetSource(0);
+            source.sourceTransform = null;
+            parentContraint.SetSource(0, source);
+            parentContraint.weight = 0;
+            parentContraint.constraintActive = false;
+            isWorn = false;
+            attachmentPoint.wearableAttached = false;
+            attachmentPoint = null;
+        }
+    }
+
+    public void OnGrabInteractableDeactivated(SelectExitEventArgs args)
+    {
+        Debug.Log("Deactivated");
+        isInteracted = false;
+    }
 
     private void OnTriggerStay(Collider other)
     {
+        if (isWorn) return;
+        if (isInteracted) return;
+
         if (grabInteractable.attachTransform != null)
         {
             return;
@@ -25,12 +56,22 @@ public class Wearable : MonoBehaviour
 
         Debug.Log("Hat collision with: " + other.gameObject.name);
 
-        WearableAttachmentPoint attachmentPoint = other.GetComponent<WearableAttachmentPoint>();
-        if (attachmentPoint != null)
+        var wap = other.GetComponent<WearableAttachmentPoint>();
+        if (wap != null)
         {
+            if (wap.wearableAttached)
+                return;
+
+            attachmentPoint = wap;
+
             var source = parentContraint.GetSource(0);
             source.sourceTransform = attachmentPoint.transform;
             parentContraint.SetSource(0, source);
+            parentContraint.weight = 1;
+            parentContraint.constraintActive = true;
+            isWorn = true;
+            attachmentPoint.wearableAttached = true;
         }
     }
+
 }
